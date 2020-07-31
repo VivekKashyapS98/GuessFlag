f35ae4b0aff30be1c2d3175eed80494cba9a4798
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-07-31 10:24:57 +0530

    Added Changes

--- src/Form.js
+++ src/Form.js
@@ -6,7 +6,8 @@ class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            answer: ''
+            answer: '',
+            options: []
         }
         this.checkAnswer = this.checkAnswer.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -25,6 +26,7 @@ class Form extends Component {
             this.props.newQuestion();
         } else {
             alert("Wrong Answer!");
+            this.props.newQuestion();
         }
     }
 
@@ -36,22 +38,20 @@ class Form extends Component {
                 <h3>To which Country does the flag belong to:</h3>
                 <img alt="flag" src={this.props.flag}></img><br />
                 <ol>
-                    <li>
-                    <input type="radio" id={options[0]} name="counry" value={options[0]} onClick={this.handleChange} />
-                    <label for={options[0]}>{options[0]}</label>
-                    </li>
-                    <li>
-                    <input type="radio" id={options[1]} name="counry" value={options[1]} onClick={this.handleChange} />
-                    <label for={options[1]}>{options[1]}</label>
-                    </li>
-                    <li>
-                    <input type="radio" id={options[2]} name="counry" value={options[2]} onClick={this.handleChange} />
-                    <label for={options[2]}>{options[2]}</label>
-                    </li>
-                    <li>
-                    <input type="radio" id={options[3]} name="counry" value={options[3]} onClick={this.handleChange} />
-                    <label for={options[3]}>{options[3]}</label>
-                    </li>
+                    {
+                        options.map((option, ind) => (
+                            <li key={ind}>
+                                <input 
+                                    type="radio"
+                                    id={option} 
+                                    name="counry" 
+                                    value={option} 
+                                    onClick={this.handleChange} 
+                                />
+                                <label htmlFor={option}>{option}</label>
+                            </li>
+                        ))
+                    }
                 </ol>
                 <input className="check-btn" type="submit" value="check" />
             </form>
--- src/Quiz.js
+++ src/Quiz.js
@@ -18,23 +18,40 @@ class Quiz extends Component {
         if(this.state.data === undefined) {
             fetch("http://restcountries.eu/rest/v2/all")
             .then(data => data.json())
-            .then(data => this.setState({data: [data.map((val) => ({name: val.name, flag: val.flag}))]}))
-            .then(() => console.log(this.state.data));
+            .then(data => this.setState(
+                {
+                    data: data.map(({ name, flag }) => ({ name, flag }))
+                },
+                this.newQuestion
+            ))
         }
-        this.newQuestion();
     }
 
     newQuestion() {
-        const rand = Math.floor(Math.random() * 200);
-        this.setState({rand_q: {name: this.state.data[rand].name, flag: this.state.data[rand].flag}, opts: [this.state[rand-1].name, this.state[rand+1].name, this.state[rand+2].name]});
+        const rand = Math.ceil(Math.random() * 200);
+        this.setState({
+            q_no: this.state.q_no+1,
+            rand_q: this.state.data[rand],
+            opts: [
+                this.state.data[rand-1].name,
+                this.state.data[rand+1].name,
+                this.state.data[rand+2].name
+            ]
+        });
     }
 
     render() {
         let view = <h3>Loading...</h3>;
         if(this.state.data !== []) {
-            const randomObject = this.state.rand_q;
-            console.log(randomObject);
-            view = <Form newQuestion={this.newQuestion} q_no={this.state.q_no} flag={randomObject.flag} options={this.state.opts} answer={randomObject.name} />
+            const {name, flag} = this.state.rand_q;
+            console.log(name+" "+flag);
+            view = <Form 
+                        newQuestion={this.newQuestion} 
+                        q_no={this.state.q_no} 
+                        flag={flag} 
+                        options={this.state.opts} 
+                        answer={name} 
+                    />
         } 
         
         return (
